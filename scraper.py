53d10894be9aed2e73b0066093ae8b7607f95f52
bartlettde <danielbartlett19@gmail.com>
2020-10-30 16:37:41 +0000

    No connection error handling added

--- scraper.py
+++ scraper.py
@@ -5,56 +5,70 @@ from bs4 import BeautifulSoup
 def ideo_scraper():
     ideo_dict = {}
     ideo_url = 'https://www.ideo.com/jobs'
-    ideo_page = requests.get(ideo_url)
-    soup = BeautifulSoup(ideo_page.content, 'html.parser')
-    elements = soup.findAll('li', class_='JobsListing--Item')
+    try:
+        ideo_page = requests.get(ideo_url)
+        soup = BeautifulSoup(ideo_page.content, 'html.parser')
+        elements = soup.findAll('li', class_='JobsListing--Item')
 
-    for items in elements:
-        details = []
-        title_elem = items.find('span', class_='JobsListing--JobTitle')
-        location_elem = items.find('p', class_='JobsListing--Location')
-        url_elem = items.find('a', href=True)
-        details.append(title_elem.text)
-        details.append(location_elem.text)
-        details.append('https://www.ideo.com' + url_elem['href'])
-        ideo_dict[title_elem.text] = details
-    return ideo_dict
+        for items in elements:
+            details = []
+            title_elem = items.find('span', class_='JobsListing--JobTitle')
+            location_elem = items.find('p', class_='JobsListing--Location')
+            url_elem = items.find('a', href=True)
+            details.append(title_elem.text)
+            details.append(location_elem.text)
+            details.append('https://www.ideo.com' + url_elem['href'])
+            ideo_dict[title_elem.text] = details
+
+        return ideo_dict
+
+    except requests.exceptions.ConnectionError:
+        return False
 
 
 def frog_scraper():
     frog_dict = {}
     frog_url = 'https://www.frogdesign.com/careers'
-    frog_page = requests.get(frog_url)
-    soup = BeautifulSoup(frog_page.content, 'html.parser')
-    elements = soup.findAll('div', class_='StudioPositions_position__265h2')
+    try:
+        frog_page = requests.get(frog_url)
+        soup = BeautifulSoup(frog_page.content, 'html.parser')
+        elements = soup.findAll('div', class_='StudioPositions_position__265h2')
 
-    for items in elements:
-        details = []
-        title_elem = items.find('span', class_='StudioPositions_title__314h4')
-        location_elem = items.find('span', class_='StudioPositions_studio__3TeSw')
-        url_elem = items.find('a', href=True)
-        details.append(title_elem.text)
-        details.append(location_elem.text)
-        details.append('https://www.frogdesign.com' + url_elem['href'])
-        frog_dict[title_elem.text] = details
+        for items in elements:
+            details = []
+            title_elem = items.find('span', class_='StudioPositions_title__314h4')
+            location_elem = items.find('span', class_='StudioPositions_studio__3TeSw')
+            url_elem = items.find('a', href=True)
+            details.append(title_elem.text)
+            details.append(location_elem.text)
+            details.append('https://www.frogdesign.com' + url_elem['href'])
+            frog_dict[title_elem.text] = details
 
-    return frog_dict
+        return frog_dict
+
+    except requests.exceptions.ConnectionError:
+        return False
 
 
 def ammo_scraper():
     ammo_dict = {}
     ammo_url = 'https://ammunitiongroup.com/contact/'
-    ammo_page = requests.get(ammo_url)
-    soup = BeautifulSoup(ammo_page.content, 'html.parser')
-    elements = soup.findAll('div', class_='position-item')
+    try:
+        ammo_page = requests.get(ammo_url)
+        soup = BeautifulSoup(ammo_page.content, 'html.parser')
+        elements = soup.findAll('div', class_='position-item')
 
-    for items in elements:
-        details = []
-        title_elem = items.find('div', class_='inner-row')
-        location_elem = items.find('div', class_='job-location col-xs-12 col-sm-4 col-md-4 col-lg-4')
-        url_elem = items.find('a', href=True)
-        details.append(title_elem.text)
-        details.append(location_elem.text)
-        details.append(url_elem['href'])
-        ammo_dict[title_elem.text] = details
-    return ammo_dict
+        for items in elements:
+            details = []
+            title_elem = items.find('div', class_='inner-row')
+            location_elem = items.find('div', class_='job-location col-xs-12 col-sm-4 col-md-4 col-lg-4')
+            url_elem = items.find('a', href=True)
+            details.append(title_elem.text)
+            details.append(location_elem.text)
+            details.append(url_elem['href'])
+            ammo_dict[title_elem.text] = details
+
+        return ammo_dict
+
+    except requests.exceptions.ConnectionError:
+        return False
